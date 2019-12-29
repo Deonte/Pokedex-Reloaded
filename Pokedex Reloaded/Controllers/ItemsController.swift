81c3bd8350885377da1ba485437737de52eda33d
Deonte Kilgore <deontecodes@gmail.com>
2019-12-29 16:01:14 -0500

    Initial project setup.

---
+++ Pokedex Reloaded/Controllers/ItemsController.swift
@@ -0,0 +1,19 @@
+//
+//  ItemsController.swift
+//  Pokedex Reloaded
+//
+//  Created by Deonte on 12/29/19.
+//  Copyright © 2019 Deonte. All rights reserved.
+//
+
+import UIKit
+
+class ItemsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
--- Pokedex Reloaded/Controllers/PokemonController.swift
+++ Pokedex Reloaded/Controllers/PokemonController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PokemonController.swift
 //  Pokedex Reloaded
 //
 //  Created by Deonte on 12/29/19.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PokemonController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        view.backgroundColor = .systemGreen
+    }
+    
 
 }
-
